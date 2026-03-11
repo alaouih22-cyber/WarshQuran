f31a00f3f48c205d380097c88499a2a8bd2c6047
alaouih22-cyber <alaouih22@gmail.com>
2026-03-11 17:40:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-const CACHE_NAME = 'quran-v2';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './style.css', './app.js'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE = 'q-v3';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './style.css', './app.js']))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
