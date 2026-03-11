9b02929e26616e8b4565074620b3b9cae6f88912
alaouih22-cyber <alaouih22@gmail.com>
2026-03-11 09:35:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-function openPage(){
+const surahSelect = document.getElementById('surahSelect');
+const ayahText = document.getElementById('ayahText');
+const tafsirText = document.getElementById('tafsirText');
+const audioPlayer = document.getElementById('audioPlayer');
 
-let page = prompt("Numero pagina Mushaf (1-604)")
+// Carica l'elenco delle Sure
+fetch('https://api.alquran.cloud')
+    .then(res => res.json())
+    .then(data => {
+        surahSelect.innerHTML = data.data.map(s => `<option value="${s.number}">${s.name}</option>`).join('');
+    });
 
-page = page.padStart(3,"0")
+surahSelect.addEventListener('change', (e) => {
+    const id = e.target.value;
+    
+    // Testo in Warsh (Edizione: quran-wordbyword)
+    fetch(`https://api.alquran.cloud/${id}/ar.ebrahim_walk`)
+        .then(res => res.json())
+        .then(data => {
+            ayahText.innerText = data.data.ayahs.map(a => a.text).join(' ۝ ');
+            audioPlayer.src = `https://cdn.islamic.network{id}.mp3`;
+        });
 
-document.getElementById("mushaf").innerHTML =
-`<img src="https://raw.githubusercontent.com/QuranHub/quran-pages-images/main/warsh/${page}.png" style="max-width:100%">`
-
-}
+    // Esempio Tafsir (Jalalayn)
+    fetch(`https://api.alquran.cloud/${id}/ar.jalalayn`)
+        .then(res => res.json())
+        .then(data => {
+            tafsirText.innerText = data.data.ayahs[0].text + "... (Tafsir)";
+        });
+});
