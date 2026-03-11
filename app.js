20278f8db8f6d5f20ec43c07c29c4b01733b1068
alaouih22-cyber <alaouih22@gmail.com>
2026-03-11 09:26:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-const mushaf=document.getElementById("mushaf")
-
 function openPage(){
 
-let page=prompt("Numero pagina Mushaf (1-604)")
+let page = prompt("Numero pagina Mushaf (1-604)")
 
-mushaf.innerHTML=
+page = page.padStart(3,"0")
 
-`<img src="mushaf/pages/${page}.png">`
-
-}
-
-function playAyah(code){
-
-let audio=
-
-new Audio(
-"https://everyayah.com/data/Husary_Warsh_64kbps/"+code+".mp3"
-)
-
-audio.play()
-
-}
-
-async function showTafsir(surah,ayah){
-
-let res=await fetch("data/tafsir.json")
-
-let data=await res.json()
-
-let t=data.find(
-x=>x.surah==surah && x.ayah==ayah
-)
-
-document.getElementById("tafsirText").innerHTML=t.text
-
-document.getElementById("tafsirBox").style.display="block"
-
-}
-
-function closeTafsir(){
-
-document.getElementById("tafsirBox").style.display="none"
+document.getElementById("mushaf").innerHTML =
+`<img src="https://raw.githubusercontent.com/QuranHub/quran-pages-images/main/warsh/${page}.png" style="max-width:100%">`
 
 }
