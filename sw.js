af9d0ad1c84ed294d23204a3227dce7fc3deb231
alaouih22-cyber <alaouih22@gmail.com>
2026-03-11 15:47:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,48 @@
-const CACHE_NAME = 'quran-v1';
-const assets = ['/', 'index.html', 'style.css', 'app.js', 'manifest.json'];
+const CACHE_NAME = 'quran-app-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+// Installazione: metto in cache i file principali
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Attivazione: pulisco vecchie cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: provo a prendere dalla cache, altrimenti vado in rete
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
