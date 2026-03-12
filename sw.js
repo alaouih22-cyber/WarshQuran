6f10ba1ebc3d73d32f448469b0716ce8b309e8e3
alaouih22-cyber <alaouih22@gmail.com>
2026-03-12 22:36:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,36 @@
-const CACHE_NAME = 'quran-v3'; // Cambiato da v2 a v3
-const assets = ['index.html', 'manifest.json'];
+const CACHE_NAME = 'quran-warsh-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  self.skipWaiting(); // Forza l'attivazione immediata
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+// Installazione e attivazione
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Pulizia vecchia cache per attivare l'audio e le nuove funzioni
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Risposta alle richieste
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
