613b3519c623c91e1f3b0888cd69ed1700ce1727
alaouih22-cyber <alaouih22@gmail.com>
2026-03-17 13:46:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE_NAME = 'quran-pro-v4';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'quran-pro-v-final-update';
+const ASSETS = [
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+    })
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
