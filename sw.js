1c2396c26a0e98d79c72a79a49756fdb6bc9dc27
alaouih22-cyber <alaouih22@gmail.com>
2026-03-17 07:15:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,10 @@
-const CACHE_NAME = 'quran-pwa-v3';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
+const CACHE_NAME = 'quran-pro-v4';
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
